d46d70d75ea6f2e4be45e632ae8a901e176c3b68
Eric P <ericpp@gmail.com>
2023-07-12 22:08:50 -0500

    Start of Reply/Boost Back frontend code

--- webroot/script/helipad.js
+++ webroot/script/helipad.js
@@ -99,9 +99,15 @@ $(document).ready(function () {
                     let boostRemotePodcast = element.remote_podcast;
                     let boostRemoteEpisode = element.remote_episode;
                     let boostTlv = {};
+                    let boostReplyAddress;
+                    let boostReplyCustomKey;
+                    let boostReplyCustomValue;
 
                     try {
                         boostTlv = JSON.parse(element.tlv)
+                        boostReplyAddress = boostTlv.reply_address;
+                        boostReplyCustomKey = boostTlv.reply_custom_key || '';
+                        boostReplyCustomValue = boostTlv.reply_custom_value || '';
                     }
                     catch {}
 
@@ -124,6 +130,28 @@ $(document).ready(function () {
                         boostMessage = '' +
                             '      <hr>' +
                             '      <p>' + boostMessage + '</p>';
+                    }
+
+                    let boostReply = '';
+                    if (boostReplyAddress) {
+                        boostReply = `
+                        <a
+                          href="#"
+                          class="btn btn-sm btn-outline-primary pull-right position-relative"
+                          style="top: -5px"
+                          data-toggle="modal"
+                          data-target="#replyModal"
+                          data-index="${boostIndex}"
+                          data-reply-address="${boostReplyAddress}"
+                          data-reply-custom-key="${boostReplyCustomKey}"
+                          data-reply-custom-value="${boostReplyCustomValue}"
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor">
+                            <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path d="M205 34.8c11.5 5.1 19 16.6 19 29.2v64H336c97.2 0 176 78.8 176 176c0 113.3-81.5 163.9-100.2 174.1c-2.5 1.4-5.3 1.9-8.1 1.9c-10.9 0-19.7-8.9-19.7-19.7c0-7.5 4.3-14.4 9.8-19.5c9.4-8.8 22.2-26.4 22.2-56.7c0-53-43-96-96-96H224v64c0 12.6-7.4 24.1-19 29.2s-25 3-34.4-5.4l-160-144C3.9 225.7 0 217.1 0 208s3.9-17.7 10.6-23.8l160-144c9.4-8.5 22.9-10.6 34.4-5.4z"/>
+                          </svg>
+                          Reply
+                        </a>`;
                     }
 
                     //If there is a difference between actual and stated sats, display it
@@ -152,6 +180,7 @@ $(document).ready(function () {
                         let dateTime = new Date(element.time * 1000).toISOString();
                         $('div.nodata').remove();
 
+
                         //Build the message element
                         elMessage = '' +
                             '<div class="outgoing_msg message" data-msgid="' + boostIndex + '">' +
@@ -162,6 +191,7 @@ $(document).ready(function () {
                             '      <time class="time_date" datetime="' + dateTime + '" title="' + dateFormat(dateTime) + '">' + 
                             '        <a href="#" style="color: blue" data-toggle="modal" data-target="#boostInfo">' + prettyDate(dateTime) + '</a>' + 
                             '      </time>' +
+                            '      ' + boostReply +
                             '      <small class="podcast_episode">' +
                             '        ' + boostPodcast + ' - ' + boostEpisode +
                             '        <span class="remote_item">' + boostRemoteInfo + '</span>' +
@@ -435,9 +465,83 @@ $(document).ready(function () {
         });
     }
 
+    function renderReplyModal() {
+        const $dialog = $(`
+        <div id="replyModal" class="modal" tabindex="-1">
+          <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Reply to Boost</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="form-group row">
+                    <label for="recipient-address" class="col-sm-2 col-form-label">Recipient:</label>
+                    <div id="recipient-address" class="col-sm-10 col-form-label text-truncate">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="sender-name" class="col-sm-2 col-form-label">Sender:</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="sender-name" placeholder="anonymous">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="sat-amt" class="col-sm-2 col-form-label">Sats:</label>
+                    <div class="col-sm-10">
+                      <input type="number" class="form-control w-auto" id="sat-amt" placeholder="5000">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="message-text" class="col-form-label">Message:</label>
+                    <textarea class="form-control" id="message-text" style="height: 8rem;" maxlength="500"></textarea>
+                    <span id="message-chars">500</span> characters remaining
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <input id="reply-index" type="hidden" name="index" value="">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button id="send-boost-reply" type="button" class="btn btn-primary d-flex align-items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor" style="margin-right: 0.25rem">
+                    <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                    <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
+                  </svg>
+                  Boost
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>`).appendTo('body');
+
+        $dialog.on('show.bs.modal', function (ev) {
+            const data = $(ev.relatedTarget).data();
+            $dialog.find('#reply-index').val(data.index);
+            $dialog.find('#recipient-address').text(data.replyAddress);
+            $dialog.find('#sender-name').val('');
+            $dialog.find('#sat-amt').val('');
+            $dialog.find('#message-text').val('');
+            $dialog.find('#message-chars').text(
+                $dialog.find('#message-text').prop('maxLength')
+            );
+        });
+
+        $dialog.find('#message-text').on('change keydown keyup', function () {
+            $dialog.find('#message-chars').text(this.maxLength - this.value.length);
+        });
+
+        $dialog.find('#send-boost-reply').click(function () {
+            $(this).text('Boosting')
+        });
+    }
+
     //Build the UI with the page loads
     async function initPage() {
         setConfig();
+        renderReplyModal();
         //Get starting balance and index number
         getBalance(true);
         await getAppList();
