bf8f8e7282e0e941cc0b921bb17f1d7989c3a778
Eric P <ericpp@gmail.com>
2023-08-01 21:06:53 -0500

    Added boost form validation and catch XHR errors

--- webroot/script/helipad.js
+++ webroot/script/helipad.js
@@ -470,14 +470,14 @@ $(document).ready(function () {
         <div id="replyModal" class="modal" tabindex="-1">
           <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Reply to Boost</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form>
+              <form id="reply-form" class="needs-validation">
+                <div class="modal-header">
+                  <h5 class="modal-title">Reply to Boost</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
                   <div class="form-group row">
                     <label for="recipient-name" class="col-sm-2 col-form-label">Recipient:</label>
                     <div id="recipient-name" class="col-sm-10 col-form-label text-truncate">
@@ -492,7 +492,10 @@ $(document).ready(function () {
                   <div class="form-group row">
                     <label for="sat-amt" class="col-sm-2 col-form-label">Sats:</label>
                     <div class="col-sm-10">
-                      <input type="number" class="form-control w-auto" id="sat-amt" placeholder="5000">
+                      <input type="number" class="form-control w-auto" id="sat-amt" placeholder="sats" required>
+                      <div class="invalid-feedback">
+                        Please enter the number of sats to send.
+                      </div>
                     </div>
                   </div>
                   <div class="form-group">
@@ -500,20 +503,20 @@ $(document).ready(function () {
                     <textarea class="form-control" id="message-text" style="height: 8rem;" maxlength="500"></textarea>
                     <span id="message-chars">500</span> characters remaining
                   </div>
-                </form>
-              </div>
-              <div class="modal-footer">
-                <input id="reply-index" type="hidden" name="index" value="">
-                <input id="recipient-address" type="hidden" name="recipient-address" value="">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button id="send-boost-reply" type="button" class="btn btn-primary d-flex align-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor" style="margin-right: 0.25rem">
-                    <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
-                  </svg>
-                  Boost
-                </button>
-              </div>
+                </div>
+                <div class="modal-footer">
+                  <input id="reply-index" type="hidden" name="index" value="" required>
+                  <input id="recipient-address" type="hidden" name="recipient-address" value="">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button id="send-boost-reply" type="button" class="btn btn-primary d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor" style="margin-right: 0.25rem">
+                      <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                      <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
+                    </svg>
+                    Boost
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>`).appendTo('body');
@@ -534,15 +537,26 @@ $(document).ready(function () {
             $.get(`/api/v1/nodeAlias?pubkey=${data.replyAddress}`, function (result) {
                 $dialog.find('#recipient-name').text(result);
             })
+
+            $dialog.find('#reply-form').removeClass('was-validated');
         });
 
         $dialog.find('#message-text').on('change keydown keyup', function () {
             $dialog.find('#message-chars').text(this.maxLength - this.value.length);
         });
 
-        $dialog.find('#send-boost-reply').click(function () {
+        $dialog.find('#send-boost-reply').click(function (event) {
+            const $form = $dialog.find('#reply-form');
+
+            if (!$form[0].checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            $form.addClass('was-validated');
+
             const $btn = $(this);
-            $btn.text('Boosting');
+            $btn.text('Boosting').prop('disabled', true);
 
             $.post(`/api/v1/reply`, {
                 // index: $dialog.find('#reply-index').val(),
@@ -555,9 +569,12 @@ $(document).ready(function () {
                     return alert(result.message);
                 }
 
-                $btn.text('Boosted!').prop('disabled', true);
+                $btn.text('Boosted!');
 
                 setTimeout(() => $dialog.modal('hide'), 1000);
+            }).fail(function (req) {
+                alert(req.responseText)
+                $btn.text('Boost').prop('disabled', false);
             });
         });
     }
