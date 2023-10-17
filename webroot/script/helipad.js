8a4e5b0da43b0597c54d3062ea9c0aa990fec08f
Eric P <ericpp@gmail.com>
2023-10-17 01:30:15 -0500

    fix boost positioning

--- webroot/script/helipad.js
+++ webroot/script/helipad.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
                         <a
                           href="#"
                           class="btn btn-sm btn-outline-primary pull-right position-relative"
-                          style="top: -5px"
+                          style="top: -5px; display: inline-flex; align-items: center;"
                           data-toggle="modal"
                           data-target="#replyModal"
                           data-index="${boostIndex}"
@@ -146,11 +146,11 @@ $(document).ready(function () {
                           data-reply-custom-key="${boostReplyCustomKey}"
                           data-reply-custom-value="${boostReplyCustomValue}"
                         >
-                          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor" style="margin-right: 0.25rem">
+                          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="currentColor" style="margin-right: 0.375rem">
                             <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                             <path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
                           </svg>
-                          Boost
+                          Reply
                         </a>`;
                     }
 
@@ -182,24 +182,34 @@ $(document).ready(function () {
 
 
                         //Build the message element
-                        elMessage = '' +
-                            '<div class="outgoing_msg message" data-msgid="' + boostIndex + '">' +
-                            '  <div class="sent_msg">' +
-                            '    <div class="sent_withd_msg">' +
-                            '      <span class="app"><a href="' + appIconHref + '"><img src="' + appIconUrl + '" title="' + boostApp + '" alt="' + boostApp + '"></a></span>' +
-                            '      <h5 class="sats">' + boostDisplayAmount + ' ' + boostPerson + ' ' + boostNumerology + '</small></h5>' +
-                            '      <time class="time_date" datetime="' + dateTime + '" title="' + dateFormat(dateTime) + '">' + 
-                            '        <a href="#" style="color: blue" data-toggle="modal" data-target="#boostInfo">' + prettyDate(dateTime) + '</a>' + 
-                            '      </time>' +
-                            '      ' + boostReply +
-                            '      <small class="podcast_episode">' +
-                            '        ' + boostPodcast + ' - ' + boostEpisode +
-                            '        <span class="remote_item">' + boostRemoteInfo + '</span>' +
-                            '      </small>' +
-                            boostMessage
-                        '    </div>' +
-                        '  </div>' +
-                        '</div>';
+                        elMessage = `
+                        <div class="outgoing_msg message" data-msgid="${boostIndex}" style="width: 100%">
+                          <div class="sent_msg">
+                            <div class="sent_withd_msg">
+                              <span class="app">
+                                <a href="${appIconHref}"><img src="${appIconUrl}" title="${boostApp}" alt="${boostApp}"></a>
+                              </span>
+                              <div>
+                                <time class="time_date" datetime="${dateTime}" title="${dateFormat(dateTime)}">
+                                  <a href="#" style="color: blue" data-toggle="modal" data-target="#boostInfo">
+                                    ${prettyDate(dateTime)}
+                                  </a>
+                                </time>
+                                <h5 class="sats">
+                                  ${boostDisplayAmount} ${boostPerson} ${boostNumerology}
+                                </h5>
+                                ${boostReply}
+                                <small class="podcast_episode">
+                                  ${boostPodcast} - ${boostEpisode}
+                                  <span class="remote_item">${boostRemoteInfo}</span>
+                                </small>
+                              </div>
+                              <div style="clear: both">
+                                ${boostMessage}
+                              </div>
+                            </div>
+                          </div>
+                        </div>`;
 
                         //Insert the message in the right spot
                         if (displayedMessageCount == 0) {
