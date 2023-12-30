25f506207e9c27839271b2200228f6558bffdd5f
Eric P <ericpp@gmail.com>
2023-12-30 01:16:11 -0600

    reply to idx

--- dbif/src/lib.rs
+++ dbif/src/lib.rs
@@ -615,7 +615,7 @@ pub fn add_payment_to_db(filepath: &String, boost: BoostRecord) -> Result<bool, 
             reply_to_idx
         )
         VALUES
-            (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18)
+            (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19)
         ",
         params![
             boost.index,
